b1b64091ecd1cfd961a3261b9a523e0066e939d0
EdKazik <edoskazik@gmail.com>
2023-07-01 22:47:20 +0200

    5 commit

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 function Human(name, gender) {
-  this.name = name;
-  this.gender = gender;
+    this.name = name;
+    this.gender = gender;
 }
 
 function Apartment() {
-  this.residents = [];
+    this.residents = [];
 
-  this.createResident = function(newResident) {
-    this.residents.push(Human);
-    
-  } 
+    this.createResident = function (newResident) {
+        
+        if(newResident instanceof Human === true){
+            this.residents.push(Human);
+
+        } else {
+            console.log(`Error, wrong type`);
+            }
+    }
 }
 
 function Building(maxApartment) {
-  this.maxApartment = maxApartment;
-  this.apartments = [];
+    this.maxApartment = maxApartment;
+    this.apartments = [];
+    
+    if (this.apartments > this.maxApartment) {
+        console.log(`Error, the number of apartments is too high`);
+    } else {
+        this.createApartment = function(newApartment) {
 
-if(this.apartments > this.maxApartment || this.createApartment instanceof Apartment !== false) {
-	console.log(`Error`)
-} else {
-		this.createApartment = function(newApartment) {
-    this.apartments.push(Apartment)
-		}
+        if (newApartment instanceof Apartment === true ) {
+            this.apartments.push(Apartment)
+        } else {
+            console.log(`Error`);	
+        }
 	}
-  console.log(this.apartments)
+}
 }
 
-const newBuilding = new Building(7);
+const newBuilding = new Building(3);
 const Flat = new Apartment();
 const Max = new Human('Max', 'female');
 const Lara = new Human('Lara', 'male');
 const Mark = new Human('Mark', 'female');
+
 Flat.createResident(Max);
 Flat.createResident(Lara);
+Flat.createResident(Mark);
+
 newBuilding.createApartment(Flat)
